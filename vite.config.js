74a1aeb4f090dc9d330ca71571610385ecded859
Nomso <nomsounaogu@gmail.com>
2024-07-21 09:07:16 +0100

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Netflix-React-Normal_CSS/",
   server:{
     open: true,
     port: 5173,
